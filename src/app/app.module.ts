4624fa4939256c97d5b272d55cf6f8a2de4fcbc4
chandravarma.bantupalli <chandravarma.bantupalli@cgi.com>
2019-09-14 20:51:50 +0530

    Added Modules for Each portal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DoctorPortalModule } from './doctor-portal/doctor-portal.module';
+import { DiagnosisCenterPortalModule } from './diagnosis-center-portal/diagnosis-center-portal.module';
+import { PatientPortalModule } from './patient-portal/patient-portal.module';
+import { PharmacyPortalModule } from './pharmacy-portal/pharmacy-portal.module';
+import { OnboardingPortalModule } from './onboarding-portal/onboarding-portal.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +16,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DoctorPortalModule,
+    DiagnosisCenterPortalModule,
+    PatientPortalModule,
+    PharmacyPortalModule,
+    OnboardingPortalModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
