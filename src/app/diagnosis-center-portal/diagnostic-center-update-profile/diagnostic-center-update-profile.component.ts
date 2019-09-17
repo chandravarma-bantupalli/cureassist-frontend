7ca14665a88f1b4fbc1f44e18a6e5887517be9ac
chandravarma.bantupalli <chandravarma.bantupalli@cgi.com>
2019-09-17 17:59:29 +0530

    Diagnosis Center can update it's profile

--- src/app/diagnosis-center-portal/diagnostic-center-update-profile/diagnostic-center-update-profile.component.ts
+++ src/app/diagnosis-center-portal/diagnostic-center-update-profile/diagnostic-center-update-profile.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { DiagnosticCenterHttpService } from 'src/app/services/diagnostic-center-http.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { DiagnosticCenter } from 'src/app/models/diagnostic-center';
 
 @Component({
   selector: 'app-diagnostic-center-update-profile',
@@ -7,9 +11,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DiagnosticCenterUpdateProfileComponent implements OnInit {
 
-  constructor() { }
+  dcProfile: FormGroup;
+  diagCen: DiagnosticCenter;
+
+  constructor(
+    private router: Router,
+    private dcService: DiagnosticCenterHttpService,
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.initializeDcProfileForm();
+    this.getDiagnosticCenterProfile();
+  }
+
+  initializeDcProfileForm() {
+    this.dcProfile = this.formBuilder.group({
+      ts: '',
+      diagnosticCenterId: '',
+      diagnosticCenterName: '',
+      diagnosticCenterEmail: '',
+      diagnosticCenterPhone: '',
+      diagnosticCenterCity: '',
+      diagnosticCenterAddress: '',
+      pincode: '',
+      diagnosticCenterRegNum: '',
+      testsConducted: '',
+      diagnosticCenterSlots: ''
+    });
+  }
+
+  getDiagnosticCenterProfile() {
+    this.dcService.getDiagnosticCenterById().subscribe( (data) => {
+      this.dcProfile.setValue(data);
+      this.diagCen = data;
+    });
+  }
+
+  saveProfile() {
+    const dc: DiagnosticCenter = this.dcProfile.value;
+    console.log(dc);
+    dc.testsConducted = this.diagCen.testsConducted;
+    dc.diagnosticCenterSlots = this.diagCen.diagnosticCenterSlots;
+    // setting the above values to previous because, these are updated only during the CRUD operations on Time Slots.
+    this.dcService.updateDiagnosticCenter(dc).subscribe( (res) => {
+      console.log(res);
+    });
+    this.router.navigate(['/diagnosisCenter/profile']);
   }
 
 }
