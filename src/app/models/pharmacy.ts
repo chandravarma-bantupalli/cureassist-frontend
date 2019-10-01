4656411d53be257396e2f7d9919f23bd98180927
Anand u <anand.u@cgi.com>
2019-10-01 09:41:08 +0530

    added filter by pincode

--- src/app/models/pharmacy.ts
+++ src/app/models/pharmacy.ts
@@ -1,6 +1,7 @@
 export class Pharmacy {
   pharmacyName: string;
   pharmacyLocation: string;
+  pharmacyPincode: string;
   pharmacyRegisterNumber: string;
   EmailId: string;
   phoneNumber: Int32Array;
